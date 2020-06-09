46c0798f045dd84011909307a39b4be530d4b3ae
unknown <kulesza.michal@gmail.com>
2020-06-09 12:52:32 +0200

    added main header

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./Main.scss";
 
 const Main = () => {
-	return <>hello</>;
+	return (
+		<div className="main">
+			<section className="header">
+				<div className="header__container">
+					<div className="header__title">
+						Recipes for All Tastes and Occasions
+					</div>
+					<div className="header__description">
+						Choose from over 2,500 lunch or dinner recipes paired with easy,
+						step-by-step instructions specially created by our chefs.
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default Main;
