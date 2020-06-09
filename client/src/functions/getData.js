4191a46a7b2d0956e6b26e4f26b4bb6424a712a7
unknown <kulesza.michal@gmail.com>
2020-06-09 14:19:56 +0200

    get mostFavourited data

--- client/src/functions/getData.js
+++ client/src/functions/getData.js
@@ -1,9 +1,12 @@
 import data from "../recipes.json";
 
 export const mostFavourited = () => {
-	let arr = [];
-
-	return data;
+	let arr = data.sort((a, b) => {
+		if (a.favorites > b.favorites) return -1;
+		if (a.favorites < b.favorites) return 1;
+		return 0;
+	});
+	return arr;
 };
 
 export default { mostFavourited };
