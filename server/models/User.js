e98304050ce7a0d9981814424aeef1bbf0d55719
unknown <kulesza.michal@gmail.com>
2020-06-10 21:45:04 +0200

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 6,
+	},
+	recipes: [
+		{
+			_id: String,
+			favourited: Boolean,
+			rating: Number,
+		},
+	],
+});
+
+// Hash the password before saving the user model
+userSchema.pre("save", async function (next) {
+	const user = this;
+	if (user.isModified("password")) {
+		user.password = await bcrypt.hash(user.password, 10);
+	}
+	next();
+});
+
+userSchema.methods.generateAuthToken = async function () {
+	const user = this;
+	const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+	user.tokens = user.tokens.concat({ token });
+	await user.save();
+	return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new Error("Invalid login credentials");
+	}
+	const isPasswordMatch = await bcrypt.compare(password, user.password);
+	if (!isPasswordMatch) {
+		throw new Error("Invalid login credentials");
+	}
+	return user;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = UserData;
