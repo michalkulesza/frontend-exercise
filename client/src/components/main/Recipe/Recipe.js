a0692bf262494e108f81fecdb1023cd9595471d5
unknown <kulesza.michal@gmail.com>
2020-06-10 11:24:04 +0200

    added nutriton info

--- client/src/components/main/Recipe/Recipe.js
+++ client/src/components/main/Recipe/Recipe.js
@@ -27,7 +27,24 @@ const Recipe = ({
 				<div>
 					<div className="title">{name}</div>
 					<div className="subtitle">{subtitle}</div>
-					<div className="info"></div>
+					<div className="info">
+						<div className="info-container">
+							<div className="info-value">900kcal</div>
+							<div className="info-name">Calories</div>
+						</div>
+						<div className="info-container">
+							<div className="info-value">3g</div>
+							<div className="info-name">Fat</div>
+						</div>
+						<div className="info-container">
+							<div className="info-value">5g</div>
+							<div className="info-name">Carbs</div>
+						</div>
+						<div className="info-container">
+							<div className="info-value">10g</div>
+							<div className="info-name">Protein</div>
+						</div>
+					</div>
 					<div className="rating">
 						<Rating rating={rating} />
 					</div>
