ef003f02f4534a41e49c21046fa9d09ef5684617
unknown <kulesza.michal@gmail.com>
2020-06-10 16:41:41 +0200

    button link is dynamic

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Navbar.scss";
+import { useLocation, Link } from "react-router-dom";
 
 import { ReactComponent as Reactlogo } from "../../assets/HelloFresh_Logo_Horizontal_V2.svg";
 
 const Navbar = () => {
+	const location = useLocation();
+	let currentLocation = location.pathname;
+
 	return (
 		<div className="navbar">
 			<div className="left">
@@ -22,7 +26,15 @@ const Navbar = () => {
 				</nav>
 			</div>
 			<div className="right">
-				<button className="navbar__right-button">Log in</button>
+				{currentLocation === "/login" ? (
+					<Link to="/register">
+						<button className="navbar__right-button">Register</button>
+					</Link>
+				) : (
+					<Link to="/login">
+						<button className="navbar__right-button">Log in</button>
+					</Link>
+				)}
 			</div>
 		</div>
 	);
