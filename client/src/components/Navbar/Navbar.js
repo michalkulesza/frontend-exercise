42395ff84ed54a481142529b316c613d7de8835b
unknown <kulesza.michal@gmail.com>
2020-06-11 15:06:37 +0200

    responsive mobile menu

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { useLocation, Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import { ReactComponent as Reactlogo } from "../../assets/HelloFresh_Logo_Horizontal_V2.svg";
 
+import { FiMenu } from "react-icons/fi";
+import { GrClose } from "react-icons/gr";
+
 const Navbar = ({ token, setToken, setUserData }) => {
+	const [mobileMenu, setMobileMenu] = useState(false);
 	const location = useLocation();
 	let currentLocation = location.pathname;
 
@@ -28,6 +32,10 @@ const Navbar = ({ token, setToken, setUserData }) => {
 			.catch(err => console.error(err));
 	};
 
+	const handleMobileMenu = () => {
+		setMobileMenu(!mobileMenu);
+	};
+
 	return (
 		<div className="navbar">
 			<div className="left">
@@ -47,18 +55,64 @@ const Navbar = ({ token, setToken, setUserData }) => {
 			</div>
 			<div className="right">
 				{currentLocation === "/login" && !token ? (
-					<Link to="/register">
-						<button className="navbar__right-button">Register</button>
+					<Link to="/register" className="link">
+						<button className="button">Register</button>
 					</Link>
 				) : token ? (
-					<button className="navbar__right-button" onMouseDown={handleLogout}>
+					<button className="button" onMouseDown={handleLogout}>
 						Log out
 					</button>
 				) : (
-					<Link to="/login">
-						<button className="navbar__right-button">Log in</button>
+					<Link to="/login" className="link">
+						<button className="button">Log in</button>
 					</Link>
 				)}
+				<div className="menu-mobile-button" onMouseDown={handleMobileMenu}>
+					{mobileMenu ? <GrClose /> : <FiMenu />}
+				</div>
+			</div>
+			<div className={mobileMenu ? "menu-mobile visible" : "menu-mobile"}>
+				<ul>
+					<li>
+						<a href="https://hellofresh.com">Home</a>
+					</li>
+					<li>
+						<Link to="/" onMouseDown={() => setMobileMenu(false)}>
+							Recipes
+						</Link>
+					</li>
+					<li>
+						{currentLocation === "/login" && !token ? (
+							<Link to="/register">
+								<button
+									className="button"
+									onMouseDown={() => setMobileMenu(false)}
+								>
+									Register
+								</button>
+							</Link>
+						) : token ? (
+							<button
+								className="button"
+								onMouseDown={() => {
+									handleLogout();
+									setMobileMenu(false);
+								}}
+							>
+								Log out
+							</button>
+						) : (
+							<Link to="/login">
+								<button
+									className="button"
+									onMouseDown={() => setMobileMenu(false)}
+								>
+									Log in
+								</button>
+							</Link>
+						)}
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
